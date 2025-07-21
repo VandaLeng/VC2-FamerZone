c0176200a9ae9d16f67ed7bf7b4e02d9e163a892
vandaleng <vanda.leng@student.passerellesnumeriques.org>
2025-07-21 19:49:41 +0700

    feat(Feature_Authentication): added table scheme in migration file push to database [jira-18]

---
+++ backend/database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+
+        $admin = User::firstOrCreate([
+            'email' => 'vandaleng@gmail.com',
+        ], [
+            'name' => 'vandaleng',
+            'password' => bcrypt('password'),
+            'image' => 'default.jpg',
+        ]);
+
+        $admin->assignRole($adminRole);
+    }
+}
